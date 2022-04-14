f97ede83084622d607d89eae8283fe244c37cda4
Saurav <sauravkul0@gmail.com>
2022-04-14 19:51:52 +0530

    :tada::tada:Final working code ready :beers:

--- hackerRank.js
+++ hackerRank.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const {email} = require('./secrets.js');
 const {password} = require('./secrets.js');
+const {answer} = require('./codes');
 
 let curTab;
 let browserOPenPromise = puppeteer.launch({
@@ -64,7 +65,18 @@ browserOPenPromise // fulfill
     return linkArrayPromise;
 })
 .then(function (linkArr) {
-    console.log(linkArr);
+    // console.log(linkArr);
+    console.log("Link to all questions recived");
+    let questionWillBeSolvedPromise = questionSolver(linkArr[0] , 0);
+    for (let i = 1; i < linkArr.length;i++) {
+        questionWillBeSolvedPromise = questionWillBeSolvedPromise.then(function () {
+            return questionSolver(linkArr[i] , i);
+        })
+    }
+    return questionWillBeSolvedPromise;
+})
+.then(function () {
+    console.log("Question is solved");
 })
 
 .catch(function (err) {
@@ -88,5 +100,91 @@ function WaitnClick(algoBtn) {
         })
     });
     return waitandclickPromise;
+}
+
+function questionSolver(url , idx) {
+    return new Promise(function (resolve , reject) {
+        let fullLink = `https://www.hackerrank.com${url}`;
+        let gotoQuestionpromise = curTab.goto(fullLink);
+         gotoQuestionpromise
+
+         .then(function () {
+          console.log("question page opened");
+
+          let inputBoxClickedPromise = WaitnClick(".checkbox-input");
+          return inputBoxClickedPromise;
+        })
+        .then(function () {
+            console.log("Check box clicked");
+            let waitForTextBoxPromise = curTab.waitForSelector(".custominput");
+            return waitForTextBoxPromise;
+        })
+        // .then(function () {
+            // console.log("Text box found");
+            //     let textBoxClicked = curTab.click(".custominput");
+            //     return textBoxClicked;
+            // })
+            .then(function () {
+                  console.log("Text box found");
+            let codeWillBeTypedPromise = curTab.type(".custominput", answer[idx], {
+            //   delay: 100,
+            });
+            return codeWillBeTypedPromise;
+          })
+          .then(function () {
+            console.log("answer is typed in cunstom box");
+            let controlKeyIsPressed = curTab.keyboard.down("Control");
+            return  controlKeyIsPressed;
+        })
+        .then(function () {
+            console.log("Control Key pressed");
+            let aKeyIsPressed = curTab.keyboard.down("a");
+            return  aKeyIsPressed;
+          })
+        .then(function () {
+            console.log("a Key pressed");
+            let xKeyIsPressed = curTab.keyboard.down("x");
+            return  xKeyIsPressed;
+        })
+        .then(function () {
+              console.log("x Key pressed");
+            let controlKeyIsPressed = curTab.keyboard.up("Control");
+            return  controlKeyIsPressed;
+        })
+        .then(function () {
+            console.log("Control button released");
+            let cursorOnTheEditorPromise = curTab.click(".monaco-editor.no-user-select.vs");
+            return cursorOnTheEditorPromise;
+        })
+        .then(function () {
+            console.log("cursor on editor");
+            let controlKeyIsPressed = curTab.keyboard.down("Control");
+            return  controlKeyIsPressed;
+        })
+        .then(function () {
+            console.log("Control Key pressed");
+            let aKeyIsPressed = curTab.keyboard.down("a");
+            return  aKeyIsPressed;
+          })
+          .then(function () {
+            console.log("a Key pressed");
+            let vKeyIsPressed = curTab.keyboard.down("v");
+            return  vKeyIsPressed;
+          })
+          .then(function () {
+              console.log("v key pressed");
+              let sumbitBtnClickedPromise = curTab.click(".hr-monaco-submit");
+              return sumbitBtnClickedPromise;
+          })
+          .then(function(){
+              console.log("code sumbit successsfully");
+              resolve();
+          })
+
+        .catch(function (err) {
+            console.log(err);
+        });
+    });
+    
 }
 
